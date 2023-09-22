dd92c2dc45740cf722c89f5bcb32f7c5ca4c20ca
EscaleraJandel123 <134498020+EscaleraJandel123@users.noreply.github.com>
2023-09-22 20:55:45 +0800

    del

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,7 +5,7 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-
+$routes->get('/', 'Home::index');
 $routes->get('/', 'MusicPlayerController::index');
 $routes->post('/add-to-playlist', 'MusicPlayerController::addToPlaylist');
 // Add more routes as needed
